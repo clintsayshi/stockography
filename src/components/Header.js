92a1c8c66402b34f96c95d851bc96462f3c97e8e
mcclintsayshi <manakanejustin@gmail.com>
2022-05-20 20:59:15 +0200

    added a stockography logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,31 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header>
-      <nav className="container mx-auto flex justify-between items-center px-6 sm:p-0">
-        <Link to="/" className="font-mono text-lg sm:text-xl py-2 font-medium">
-          Stockography_
+      <nav className="container mx-auto flex justify-between items-center py-4 px-6 sm:px-0">
+        <Link
+          to="/"
+          className="flex items-center gap-2  font-mono text-lg sm:text-xl font-medium"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+          Stockography
         </Link>
 
         <a href="https://pixabay.com">by pixabay</a>
