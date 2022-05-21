4e9cde4eb243d5ca49088c9784d35d94afb2c4be
mcclintsayshi <manakanejustin@gmail.com>
2022-05-21 18:39:02 +0200

    removed unnecessary code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,13 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useParams,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DetailView from "./detail-view";
 import Home from "./home";
 
-export default function () {
+export default function Pages() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-
         <Route path="/:id" element={<DetailView />} />
       </Routes>
     </Router>
