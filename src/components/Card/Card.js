7011770fbe2861ac3455cb879610765b0d920874
mcclintsayshi <manakanejustin@gmail.com>
2022-05-21 18:36:40 +0200

    updated Card tests

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Card({ data }) {
   const { id, tags, previewHeight, previewWidth, previewURL } = data;
   return (
-    <Link className="relative" to={`/${id}`} key={id}>
+    <Link data-testid={`card-${id}`} className="relative" to={`/${id}`}>
       <img
         className="w-full rounded-lg h-60 object-cover"
         alt={tags}
--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,8 +1,45 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 import Card from "./Card";
+import { MemoryRouter } from "react-router-dom";
 
-it("renders successfully", () => {
-  const div = document.createElement("div");
-  render(<Card />);
+const fakeData = {
+  id: 132132,
+  tags: "nature, sunrise",
+  previewHeight: 250,
+  previewWidth: 200,
+  previewURL:
+    "https://cdn.pixabay.com/photo/2022/04/21/20/49/road-7148369_960_720.jpg",
+};
+
+afterEach(() => {
+  cleanup();
+});
+
+test("renders Card successfully", () => {
+  render(
+    <MemoryRouter>
+      <Card data={fakeData} />
+    </MemoryRouter>
+  );
+
+  const cardElement = screen.getByTestId("card-132132");
+
+  expect(cardElement).toBeInTheDocument();
+  expect(cardElement).toMatchSnapshot();
+});
+
+test("the link works as expected", () => {
+  render(
+    <MemoryRouter>
+      <Card data={fakeData} />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByRole("link")).toHaveAttribute("href", "/132132");
+  expect(screen.getByRole("img")).toHaveAttribute(
+    "src",
+    "https://cdn.pixabay.com/photo/2022/04/21/20/49/road-7148369_960_720.jpg"
+  );
+  expect(screen.getByRole("img")).toHaveAttribute("alt", "nature, sunrise");
 });
