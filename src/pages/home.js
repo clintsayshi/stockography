04d39cf5b584c55a56b8197ecbf5de2146746544
mcclintsayshi <manakanejustin@gmail.com>
2022-05-21 18:36:01 +0200

    added a few comments and added a key to each child when displaying image cards

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,29 +1,34 @@
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { BeatLoader } from "react-spinners";
-import Card from "../components/Card";
+import Card from "../components/Card/Card";
 import Header from "../components/Header";
-import QueryResults from "../components/QueryResults";
+
+const POSTS_PER_PAGE = 10;
 
 function Home() {
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
-  const [perPage, setPerPage] = useState(10);
-
-  console.log(images);
+  const [error, setError] = useState();
 
   useEffect(() => {
-    //
+    // show a loader when fetching images
     setLoading(true);
+    // asynchronous function to fetch images from pixabay API
     const fetchImages = async () => {
-      const response = await Axios.get(
-        `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_KEY}&per_page=${perPage}&page=${page}`
-      );
-
-      setImages([...images, ...response.data.hits]);
-      setLoading(false);
+      try {
+        const response = await Axios.get(
+          `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_KEY}&per_page=${POSTS_PER_PAGE}`
+        );
+        // in order to enable infinite scrolling add newly fetched images to the exisiting ones
+        setImages([...images, ...response.data.hits]);
+        // if the program gets here means there was no errors so disable loader and errors
+        setLoading(false);
+        setError(null);
+      } catch (error) {
+        setError(error);
+      }
     };
 
     fetchImages();
@@ -35,7 +40,7 @@ function Home() {
   };
 
   window.onscroll = () => {
-    // check if user scrolled to the bottom
+    // check if user scrolled to the bottom to fetch more images
     if (
       window.innerHeight + document.documentElement.scrollTop ===
       document.documentElement.offsetHeight
@@ -48,12 +53,14 @@ function Home() {
     <div className="">
       <Header />
 
-      <section className="container h-full mx-auto px-6 sm:px-6">
+      <section className="container h-full mx-auto px-4 md:px-8 lg:px-12 xl:px-16">
         <div className="gap-6 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4">
           {images.length > 0 &&
-            images.map((image) => <Card key={image.id} data={image} />)}
+            images.map((image, key) => <Card key={key} data={image} />)}
         </div>
       </section>
+
+      {loading && <BeatLoader loading={loading} color="#000" size={20} />}
     </div>
   );
 }
