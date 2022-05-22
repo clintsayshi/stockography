f13e440a4482b8c926639d8b3e90ca160f328e33
mcclintsayshi <manakanejustin@gmail.com>
2022-05-22 14:27:19 +0200

    tried to get rid of useEffect dependency error

--- src/pages/home.js
+++ src/pages/home.js
@@ -29,6 +29,7 @@ function Home() {
     // show a loader when fetching images
     setLoading(true);
     // fetch images from pixabay API
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     fetchData();
   }, [page]);
 
